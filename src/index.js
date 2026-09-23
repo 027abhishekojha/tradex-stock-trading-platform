6664a43b5b0db26a687e00a7bd3498e243d79740
027abhishekojha <abhishekojha.edu@gmail.com>
2026-09-23 15:40:27 +0530

    about page UI

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from './landing_page/Home/HomePage';
 import Signup from './landing_page/signup/Signup';
-import About from './landing_page/about/About';
+import AboutPage from './landing_page/about/AboutPage';
 import Product from './landing_page/products/Hero';
 import Pricing from './landing_page/pricing/PricingPage';
 import Support from './landing_page/support/SupportPage';
@@ -21,7 +21,7 @@ root.render(
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
       <Route path='/Signup' element={<Signup />}></Route>
-      <Route path='/About' element={<About />}></Route>
+      <Route path='/About' element={<AboutPage />}></Route>
       <Route path='/Products' element={<Product />}></Route>
       <Route path='/Pricing' element={<Pricing />}></Route>
       <Route path='/Support' element={<Support />}></Route>
